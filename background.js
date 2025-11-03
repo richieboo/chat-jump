27a184ef3bcc0d4e82cf2a43076f922b76090d59
Richie Madden <richie@richiemadden.com>
2025-11-03 18:33:31 -0500

    refactor: Simplify background script to align with MV3 best practices

--- background.js
+++ background.js
@@ -1,17 +1,6 @@
-const uninstallUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSdjSG4SJrTfl2uHBtHYF-_G1Tu7GCJYfl72zObEjs3ah5QtwA/viewform?usp=header';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.runtime.setUninstallURL(uninstallUrl, () => {
-    if (chrome.runtime.lastError) {
-      console.error('Error setting uninstall URL:', chrome.runtime.lastError);
-    }
-  });
-});
-
-chrome.runtime.onStartup.addListener(() => {
-  chrome.runtime.setUninstallURL(uninstallUrl, () => {
-    if (chrome.runtime.lastError) {
-      console.error('Error setting uninstall URL:', chrome.runtime.lastError);
-    }
-  });
-});
+try {
+  const uninstallUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSdjSG4SJrTfl2uHBtHYF-_G1Tu7GCJYfl72zObEjs3ah5QtwA/viewform?usp=header';
+  chrome.runtime.setUninstallURL(uninstallUrl);
+} catch (e) {
+  console.error('Error setting uninstall URL:', e);
+}
